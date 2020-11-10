2a314cade5325ec824fd617c13e4b94cf44450e1
akseryanin <sasha.seryanin@yandex.ru>
2020-11-10 23:08:27 +0300

    ...

--- HW1/src/test/java/ru/stqa/AppTest.java
+++ HW1/src/test/java/ru/stqa/AppTest.java
@@ -1,18 +1,15 @@
 package ru.stqa;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+/**
+ * Unit test for simple App.
+ */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     private WebDriver driver;
     @Before
     public void start()
