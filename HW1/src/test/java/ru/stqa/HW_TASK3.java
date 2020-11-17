2bfbc366a76eb9308640087d6217ab017fd5979e
akseryanin <sasha.seryanin@yandex.ru>
2020-11-17 03:05:19 +0300

    ?

--- HW1/src/test/java/ru/stqa/HW_TASK3.java
+++ HW1/src/test/java/ru/stqa/HW_TASK3.java
@@ -1,2 +1,38 @@
-package ru.stqa;public class HW_TASK3 {
+package ru.stqa;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import static org.junit.Assert.assertTrue;
+
+public class HW_TASK3 extends TestBase
+{
+    @Test
+    public void clickedAllSections() throws InterruptedException {
+        driver.get("http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+        WebElement allSections = driver.findElement(By.cssSelector("#box-apps-menu"));
+        List<WebElement> sections = allSections.findElements(By.cssSelector("#app-"));
+
+        for (WebElement section : sections) {
+            String locatorTitle = "td#content > h1";
+
+            section.click();
+            assertTrue(isElementPresent(By.cssSelector(locatorTitle)));
+            List<WebElement> refs = section.findElements(By.cssSelector(".docs ul > li"));
+            for (WebElement ref : refs){
+                ref.click();
+                assertTrue(isElementPresent(By.cssSelector(locatorTitle)));
+            }
+
+        }
+
+    }
 }
