722b29a7bcfdc1d5ac3caf3e424fe74481e2069d
akseryanin <sasha.seryanin@yandex.ru>
2020-11-24 03:01:18 +0300

    Задание 13

--- HW1/src/test/java/ru/stqa/HW_TASK9.java
+++ HW1/src/test/java/ru/stqa/HW_TASK9.java
@@ -2,23 +2,21 @@ package ru.stqa;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.Keys;
+
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
 
-import static org.junit.Assert.*;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class HW_TASK9 extends TestBase{
 
     @Test
-    public void cart(){
+    public void checkAddDeleteProducts(){
 
-        for (int i =1; i<=3;i++){
+        for (int i = 0; i < 3; ++i){
 
             driver.get("https://litecart.stqa.ru/en/");
             wait.until(titleContains("Online Store"));
@@ -28,31 +26,34 @@ public class HW_TASK9 extends TestBase{
 
             wait.until(attributeToBe(By.cssSelector("h1"), "textContent", name));
 
-            // проверяем, есть ли на странице товара список с выбором размера
+            // проверяем, есть ли на странице товара обязательная опция размера
             if (driver.findElements(By.cssSelector("option")).size() > 0){
                 Select size = new Select(driver.findElement(By.cssSelector("[name='options[Size]']")));
                 size.selectByValue("Small");
             }
 
+            //sleep(500); //костыль который иногда чинит работу
             String count = driver.findElement(By.cssSelector("#cart .quantity")).getAttribute("textContent");
-            WebElement sendButton = driver.findElement(By.cssSelector("form[name=buy_now_form] button[type=submit]"));
+            WebElement sendButton = driver.findElement(By.cssSelector("button[name=add_cart_product]"));
             wait.until(elementToBeClickable(sendButton));
-            driver.findElement(By.cssSelector("form[name=buy_now_form] button[type=submit]")).sendKeys(Keys.RETURN);
+            sendButton.click();
             wait.until(not(attributeToBe(By.cssSelector("#cart .quantity"),"textContent",count)));
         }
 
         driver.findElement(By.cssSelector("#cart .link")).click();
+        wait.until(presenceOfElementLocated(By.cssSelector(".dataTable tr")));
 
         int rows = driver.findElements(By.cssSelector(".dataTable tr")).size() - 5;
-        for (int i=1;i<=rows;i++){
+        for (int i = 0; i < rows; ++i){
 
+            //sleep(500); //костыль который иногда чинит работу
             WebElement table = driver.findElement(By.cssSelector(".dataTable"));
-            driver.findElement(By.cssSelector("button[name=remove_cart_item]")).sendKeys(Keys.RETURN);
+            wait.until(elementToBeClickable(By.cssSelector("button[name=remove_cart_item]")));
+            driver.findElement(By.cssSelector("button[name=remove_cart_item]")).click();
 
             wait.until(stalenessOf(table));
-
             // не проверяем после удаления последнего элемента, так как таблицы нет
-            if (i!=rows){
+            if (i != rows - 1){
                 wait.until(visibilityOfElementLocated(By.cssSelector(".dataTable")));
             }
 
